92e85ea5502ee02afc7fd09611590736ba5b94be
ZSully09 <zsullivan93@gmail.com>
2019-12-09 22:36:04 +0000

    Players initialize & player name intake

--- main.rb
+++ main.rb
@@ -1,1 +1,13 @@
+require './players'
+require './games'
+
+puts "Player 1 please enter your name"
+name1 = gets.chomp
+player1 = Players.new(name1)
+puts player1.name
+
+puts "Player 2 please enter your name"
+name2 = gets.chomp
+player2 = Players.new(name2)
+puts player2.name
 
---
+++ players.rb
@@ -0,0 +1,8 @@
+class Players
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    
+  end
+end
